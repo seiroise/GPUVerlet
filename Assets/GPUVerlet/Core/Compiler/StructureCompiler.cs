e59f0aea198baa5d46b3ea2a4bf5ed082df950da
祐貴 林 <y.hayashi@y-hayashi-10.Q-DOMAIN>
2018-07-04 22:05:23 +0900

    参照用のデータ構造を追加

---
+++ Assets/GPUVerlet/Core/Compiler/StructureCompiler.cs
@@ -0,0 +1,31 @@
+﻿using Seiro.GPUVerlet.Core.RefDatas;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Seiro.GPUVerlet.Core.Converters
+{
+
+	/// <summary>
+	/// 構造体を実際に使用できる状態に仕上げる
+	/// </summary>
+	public class StructureConverter
+	{
+
+		#region 外部インタフェース
+
+		/// <summary>
+		/// コンパイルして使える状態に仕上げる
+		/// </summary>
+		/// <param name="structure"></param>
+		/// <param name="materialDict"></param>
+		public void Compile(RefStructure structure, MaterialDictionary materialDict)
+		{
+			var particles = structure.GetParticles();
+			var edges = structure.GetEdges();
+		}
+
+		#endregion
+
+	}
+}
---
+++ Assets/GPUVerlet/Core/MaterialDictionary.cs
@@ -0,0 +1,19 @@
+﻿using Seiro.GPUVerlet.Core.RefDatas;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Seiro.GPUVerlet.Core
+{
+	
+	/// <summary>
+	/// IDとMaterialのペアからなる辞書
+	/// </summary>
+	[CreateAssetMenu(menuName = "Seiro/GPUVerlet/MaterialDictionary", fileName = "MaterialDictionary")]
+	public class MaterialDictionary : ScriptableObject
+	{
+		
+		[SerializeField]
+		List<RefMaterial> _items;
+
+	}
+}
