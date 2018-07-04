7fa5b9b25b17b8383921d344a464348d90582026
seiroise <seiroise@gmail.com>
2018-07-04 22:52:00 +0900

    Revert "プレビューの追加"
    This reverts commit 5ba5f829fcfcbd72c83ebb7964f6366c7fb4a966.

--- Assets/GPUVerlet/Scripts/VStructure.cs
+++ Assets/GPUVerlet/Scripts/VStructure.cs
@@ -14,14 +14,12 @@ namespace Seiro.GPUVerlet
 
         /// <summary>
         /// パーティクル
-        /// </summary>	
-        [SerializeField]
+        /// </summary>
         List<VParticle> _particles;
 
         /// <summary>
         /// エッジ
         /// </summary>
-        [SerializeField]
         List<VEdge> _edges;
 
         /// <summary>
@@ -141,20 +139,6 @@ namespace Seiro.GPUVerlet
         public IExternalStep[] GetAfterSteps()
         {
             return _afterSteps.ToArray();
-        }
-
-        /// <summary>
-        /// パーティクル全体の座標変換を行う
-        /// </summary>
-        /// <param name="translate"></param>
-        public void TranslateParticles(Matrix4x4 translate)
-        {
-            for (var i = 0; i < _particles.Count; ++i)
-            {
-                var t = _particles[i];
-                t.position = t.prevPosition = translate.MultiplyPoint3x4(t.position);
-                _particles[i] = t;
-            }
         }
 
         #endregion
