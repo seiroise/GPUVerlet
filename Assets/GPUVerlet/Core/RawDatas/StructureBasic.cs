f2907a0842138ae51db75d28c6f789a4a6a2df97
seiroise <seiroise@gmail.com>
2018-07-05 07:49:12 +0900

    名前空間の整理

---
+++ Assets/GPUVerlet/Core/RawDatas/StructureBasic.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Seiro.GPUVerlet.Core.RawDatas
+{
+
+    /// <summary>
+    /// Particleとedgeからなる構造体
+    /// </summary>
+    public class StructureBasic
+    {
+        /// <summary>
+        /// パーティクル
+        /// </summary>
+        Particle[] _particles;
+
+        /// <summary>
+        /// エッジ
+        /// </summary>
+        Edge[] _edges;
+    }
+}
