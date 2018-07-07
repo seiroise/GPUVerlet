6c8b2c567fddb629b5304c1721821fc6494ecee8
seiroise <seiroise@gmail.com>
2018-07-08 01:30:32 +0900

    一通り落ち着いたのでpackageを出力

--- Assets/GPUVerlet/Core/Scripts/RefDatas/RefStructure.cs
+++ Assets/GPUVerlet/Core/Scripts/RefDatas/RefStructure.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -12,8 +13,13 @@ namespace Seiro.GPUVerlet.Core.RefDatas
     public class RefStructure : ScriptableObject
     {
 
-        List<RefParticle> _particles;
-        List<RefEdge> _edges;
+        [SerializeField, HideInInspector]
+        List<RefParticle> _particles = null;
+
+        [SerializeField, HideInInspector]
+        List<RefEdge> _edges = null;
+
+        [SerializeField, HideInInspector]
         uint _uidCounter = 0;
 
         #region static 外部インタフェース
@@ -45,6 +51,26 @@ namespace Seiro.GPUVerlet.Core.RefDatas
             var p = new RefParticle(GetNextUID(), position, size, color, materialID);
             _particles.Add(p);
             return p;
+        }
+
+        /// <summary>
+        /// 指定したuidに紐付いているパーティクルを削除する
+        /// </summary>
+        /// <param name="uid"></param>
+        public void RemoveParticle(RefParticle p)
+        {
+
+            if (p == null)
+            {
+                return;
+            }
+
+            // 紐付いているエッジを削除する
+            var result = FindEdgesWithParticle(p.uid);
+            for (var i = 0; i < result.Count; ++i)
+            {
+                _edges.Remove(result[i]);
+            }
         }
 
         /// <summary>
@@ -83,15 +109,24 @@ namespace Seiro.GPUVerlet.Core.RefDatas
         }
 
         /// <summary>
+        /// 指定したUIDに紐付いているエッジを削除する
+        /// </summary>
+        /// <param name="uid"></param>
+        public void RemoveEdge(RefEdge edge)
+        {
+            _edges.Remove(edge);
+        }
+
+        /// <summary>
         /// パーティクル全体の座標変換を行う
         /// </summary>
-        /// <param name="translate"></param>
-        public void TranslateParticles(Matrix4x4 translate)
+        /// <param name="translation"></param>
+        public void TranslateParticles(Matrix4x4 translation)
         {
             for (var i = 0; i < _particles.Count; ++i)
             {
                 var t = _particles[i];
-                var pos = translate.MultiplyPoint3x4(t.position);
+                var pos = translation.MultiplyPoint3x4(t.position);
                 t.position = pos;
             }
         }
@@ -112,6 +147,84 @@ namespace Seiro.GPUVerlet.Core.RefDatas
         public RefEdge[] GetEdges()
         {
             return _edges.ToArray();
+        }
+
+        /// <summary>
+        /// 使用しているMaterialIDの配列を返す
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetUsingMaterialIDs()
+        {
+            var p = GetParticleUsingMaterialIDs();
+            var e = GetEdgeUsingMaterialIDs();
+
+            var mergedArray = new string[p.Length + e.Length];
+            p.CopyTo(mergedArray, 0);
+            e.CopyTo(mergedArray, p.Length);
+
+            return mergedArray;
+        }
+
+        /// <summary>
+        /// パーティクルで使用するMaterialIDの配列を返す
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetParticleUsingMaterialIDs()
+        {
+            var dst = new HashSet<string>();
+            for (var i = 0; i < _particles.Count; ++i)
+            {
+                dst.Add(_particles[i].materialID);
+            }
+            return dst.ToArray();
+        }
+
+        /// <summary>
+        /// エッジで使用するMaterialIDの配列を返す
+        /// </summary>
+        /// <returns></returns>
+        public string[] GetEdgeUsingMaterialIDs()
+        {
+            var dst = new HashSet<string>();
+            for (var i = 0; i < _edges.Count; ++i)
+            {
+                dst.Add(_edges[i].materialID);
+            }
+            return dst.ToArray();
+        }
+
+        /// <summary>
+        /// Boundsを計算する、particleのsizeを考慮する
+        /// </summary>
+        /// <returns></returns>
+        public Bounds CalcurateBounds()
+        {
+            if (_particles.Count == 0)
+            {
+                return default(Bounds);
+            }
+
+            var first = _particles[0];
+            if (_particles.Count == 1)
+            {
+                return new Bounds(first.position, new Vector3(first.size, first.size));
+            }
+
+            Vector3 min, max;
+            min = max = first.position;
+
+            for (var i = 0; i < _particles.Count; ++i)
+            {
+                var t = _particles[i].position;
+                var hs = _particles[i].size * 0.5f;
+
+                if (min.x > t.x - hs) min.x = t.x - hs;
+                if (min.y > t.y - hs) min.y = t.y - hs;
+                if (max.x < t.x + hs) max.x = t.x + hs;
+                if (max.y < t.y + hs) max.y = t.y + hs;
+            }
+
+            return new Bounds((min + max) * 0.5f, max - min);
         }
 
         #endregion
@@ -145,6 +258,26 @@ namespace Seiro.GPUVerlet.Core.RefDatas
             Assert.IsTrue(ContainsParticlesIdx(idx));
 
             return _particles[idx];
+        }
+
+        /// <summary>
+        /// 指定したUIDに紐付いているパーティクルを含むエッジを全て探索する
+        /// </summary>
+        /// <param name="uid"></param>
+        /// <returns></returns>
+        List<RefEdge> FindEdgesWithParticle(uint uid)
+        {
+            var dst = new List<RefEdge>();
+
+            for (var i = 0; i < _edges.Count; ++i)
+            {
+                if (_edges[i].ContainsParticle(uid))
+                {
+                    dst.Add(_edges[i]);
+                }
+            }
+
+            return dst;
         }
 
         /// <summary>
